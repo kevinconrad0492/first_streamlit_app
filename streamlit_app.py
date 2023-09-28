41db72e5d202d420eb34eb1dc4115b84b56cbf8b
kevinconrad0492 <kevinconrad@hotmail.nl>
2023-09-28 09:50:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.title('My parents new healthy diner')
 
@@ -14,5 +15,5 @@ streamlit.text('🥑 Advocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
